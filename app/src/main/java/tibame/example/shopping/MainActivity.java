935752489ad24224507c92e364d2d3be19a6b561
leolin310148 <leolin310148@gmail.com>
2016-03-19 10:12:37 +0800

    login with fb and firebase

--- app/src/main/java/tibame/example/shopping/MainActivity.java
+++ app/src/main/java/tibame/example/shopping/MainActivity.java
@@ -5,23 +5,30 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private CallbackManager callbackManager;
+    private AccessTokenTracker accessTokenTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         FacebookSdk.sdkInitialize(getApplicationContext());
+        Firebase.setAndroidContext(getApplicationContext());
 
         setContentView(R.layout.activity_main);
 
@@ -43,6 +50,47 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "登入失敗", Toast.LENGTH_SHORT).show();
             }
         });
+
+        final Firebase firebase = new Firebase("https://tibame-0312-leo.firebaseio.com");
+
+
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                if (currentAccessToken == null) {
+                    Toast.makeText(MainActivity.this, "還沒登入facebook", Toast.LENGTH_SHORT).show();
+                    firebase.unauth();
+                }else{
+                    Toast.makeText(MainActivity.this, "已經登入facebook", Toast.LENGTH_SHORT).show();
+
+                    firebase.authWithOAuthToken("facebook", currentAccessToken.getToken(), new Firebase.AuthResultHandler() {
+                        @Override
+                        public void onAuthenticated(AuthData authData) {
+                            Toast.makeText(MainActivity.this, "已經登入firebase with facebook", Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onAuthenticationError(FirebaseError firebaseError) {
+                            Toast.makeText(MainActivity.this, "登入firebase失敗", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        };
+
+
+
+        firebase.addAuthStateListener(new Firebase.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(AuthData authData) {
+                if (authData == null) {
+                    Toast.makeText(MainActivity.this, "還沒登入firebase", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "已經登入firebase", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
 
     }
 
